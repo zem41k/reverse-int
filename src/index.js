63607615f8a49f1ad7f63e740ddebbb49fe80e13
Mihail Samofalov <rhbkickshot@gmail.com>
2022-02-06 14:19:12 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = String(Math.abs(n));
+    let resStr = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        resStr = `${resStr}${str[i]}`;
+    }
+    let resNum = Number(resStr);
+    return resNum;  
 }
